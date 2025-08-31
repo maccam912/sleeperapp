708890cea018e3903996cf4d984a390b3692d471
Matthew Koski <maccam912@gmail.com>
2025-08-30 21:39:19 -0400

    refactor: remove binary assets

---
+++ routes/index.tsx
@@ -0,0 +1,18 @@
+import { useSignal } from "@preact/signals";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  const count = useSignal(3);
+  return (
+    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <h1 class="text-4xl font-bold my-6">Welcome to Fresh</h1>
+        <p class="my-4">
+          Try updating this message in the
+          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+        </p>
+        <Counter count={count} />
+      </div>
+    </div>
+  );
+}
